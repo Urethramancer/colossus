7c18dc86780be38d518f947911a663e0c7e028ce
Ronny Bangsund <github@neuralshortcircuit.com>
2020-02-09 16:24:05 +0100

    Middleware to make the API entries work.

---
+++ middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func addJSONHeaders(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
+func addCORS(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
