7e11c66384f654bfc8a1bff2ea8daa75e203f277
Ronny Bangsund <github@neuralshortcircuit.com>
2020-07-05 17:56:17 +0200

    Envvars for AWS-like environments.

---
+++ internal/srv/env.go
@@ -0,0 +1,10 @@
+package srv
+
+const (
+	// ENVHOST is the address to bind to.
+	ENVHOST   = "WEBHOST"
+	ENVPORT   = "WEBPORT"
+	ENVDATA   = "DATAPATH"
+	ENVSTATIC = "STATICPATH"
+	ENVSHARE  = "SHAREPATH"
+)
